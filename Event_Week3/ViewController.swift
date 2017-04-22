dbd1e833f3cd1a3e44946c7116bc1faa2dcb83a9
Thanh Duy <duybvt95@gmail.com>
2017-04-22 14:18:33 +0700

    Initial Commit

---
+++ Event_Week3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Event_Week3
+//
+//  Created by Cntt03 on 4/22/17.
+//  Copyright © 2017 Cntt03. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
